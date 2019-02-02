42aba845b31cce5c094880ddd1d79b3eea3d5c80
Paul Ashour <paul.ashour@gmail.com>
2019-02-02 18:06:01 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
   
   def self.most_popular_show
-    Show.highest_rating.take
+    Show.where(":rating = ?", highest_rating).take
   end
   
   def self.lowest_rating
